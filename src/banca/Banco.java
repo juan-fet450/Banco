689fa33820a2af024b72cf8430da35cc0ba229ae
juan osorio <juan.osorio3@correo.usa.edu.co>
2020-08-25 20:57:06 -0500

    Creacion de metodos en la clase banco y cliente

--- src/banca/Banco.java
+++ src/banca/Banco.java
@@ -5,46 +5,193 @@
  */
 package banca;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
  * @author Juan Osorio
+ * @since 8/25/2020
  */
 public class Banco {
+    
     private HashMap<Double,Cliente> cuentasAhorros = new HashMap();
+    private HashMap<Double,Double> saldoAhorros = new HashMap();
     private HashMap<Double,Cliente> cuentasCorriente = new HashMap();
+    private HashMap<Double,Double> saldoCorriente = new HashMap();
     private HashMap<Double,Cliente> tarjetaDebito = new HashMap();
     private HashMap<Double,Cliente> tarjetaCredito = new HashMap();
     private HashMap<Double,Cliente> prestamos = new HashMap();
+    private ArrayList<Double> Fondos = new ArrayList();
     
-    public void crearCuentaAhorros(Cliente cliente){
+    public void crearCuentaAhorros(double num, Cliente cliente){
+        
+        this.cuentasAhorros.put(num, cliente);
+        
+        this.Fondos.add(num);
         
     }
-    public void crearCuentaCorriente(Cliente cliente){
+    public void crearCuentaCorriente(double num, Cliente cliente){
+        
+        this.cuentasCorriente.put(num, cliente);
+        
+        this.Fondos.add(num);
         
     }
-    public void crearTarjetaDebito(Cliente cliente){
+    public void crearTarjetaDebito(double num, Cliente cliente){
+        
+        this.tarjetaDebito.put(num, cliente);
         
     }
-    public void crearTarjetaCredito(Cliente cliente){
+    public void crearTarjetaCredito(double num, Cliente cliente){
+        
+        this.tarjetaCredito.put(num, cliente);
         
     }
-    public void crearPrestamo(Cliente cliente){
+    public double fondosTotales(){
+        
+        double ans = 0;
+        
+        for (Double Fondo : this.Fondos) {
+            
+           ans += Fondo; 
+            
+        }
+        
+        return ans;
         
     }
-    public boolean consignar(double numCuenta){
-        return false;
+    public void crearPrestamo(double num, Cliente cliente){
+        
+        
+        for (Map.Entry<Double, Cliente> entry : this.cuentasCorriente.entrySet()) {
+            
+            if(entry.getValue() == cliente){
+                
+                this.prestamos.put(num, cliente);
+                
+            }
+            
+        }
+        
+    }
+    public boolean consignar(double numCuenta, double monto){
+        
+        double aux;
+        
+        boolean var = false;
+        
+        for (Map.Entry<Double, Cliente> entry : this.cuentasAhorros.entrySet()) {
+            
+            if(entry.getKey().equals(numCuenta)){
+                
+                for (Map.Entry<Double, Double> entry1 : this.saldoAhorros.entrySet()) {
+                    
+                    aux = entry1.getValue() + monto;
+                    
+                    this.saldoAhorros.replace(numCuenta, aux);
+                    
+                    var = true;
+                }
+                
+            }else{
+                
+                var = false;
+            
+            }
+            
+        }
+        
+        return var;
     }
     
-    public double retirarDinero(String clave){
-        return 0;
+    public double retirarDinero(String clave, String id, double monto){
+        
+        double dinero = 0;
+        
+        for (Map.Entry<Double, Cliente> entry : this.cuentasAhorros.entrySet()) {
+            
+            if(entry.getValue().getCedula().equals(id)){
+                
+                if (entry.getValue().getClave().equals(clave)) {
+                    
+                    for (Map.Entry<Double, Double> entry1 : this.saldoAhorros.entrySet()) {
+                        
+                        if(entry.getKey().equals(entry1.getKey())){
+                            
+                            dinero = entry1.getValue() - monto;
+                            
+                        }
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
+        return dinero;
+        
     }
-    public double consultarSaldo(String clave){
-        return 0;
+    public double consultarSaldo(String clave, String id){
+        
+        double saldo = 0;
+        
+        for (Map.Entry<Double, Cliente> entry : this.cuentasAhorros.entrySet()) {
+            
+            if (entry.getValue().getCedula().equals(id)) {
+                
+                if(entry.getValue().getClave().equals(clave)){
+                    
+                    for (Map.Entry<Double, Double> entry1 : this.saldoAhorros.entrySet()) {
+                        
+                        if (entry.getKey().equals(entry1.getKey())) {
+                            
+                            saldo = entry1.getValue();
+                            
+                        }
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
+        return saldo;
     }
-    public boolean realizarCompra(String clave){
-        return false;
+    public boolean realizarCompra(String clave, String id, double monto){
+        
+        boolean var = false;
+        
+        for (Map.Entry<Double, Cliente> entry : this.cuentasAhorros.entrySet()) {
+            
+            if(entry.getValue().getCedula().equals(id)){
+                
+                if (entry.getValue().getClave().equals(clave)) {
+                    
+                    for (Map.Entry<Double, Double> entry1 : this.saldoAhorros.entrySet()) {
+                        
+                        if(entry.getKey().equals(entry1.getKey())){
+                            
+                            this.saldoAhorros.replace(entry1.getKey(), entry1.getValue() - monto);
+                            
+                            var = true;
+                            
+                        }
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        
+        return var;
     }
     public double consultarSaldoTarjeta(String clave){
         return 0;   
--- src/banca/Cliente.java
+++ src/banca/Cliente.java
@@ -5,30 +5,104 @@
  */
 package banca;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  *
  * @author Juan Osorio
+ * @since 8/25/2020
  */
 public class Cliente {
     private String nombre;
     private String cedula;
     private String direccion;
     private String clave;
+    private Set<Double> numCuenta = new HashSet<>();
 
+    private Banco bank = new Banco();
+    
     public Cliente(String nombre, String cedula, String direccion, String clave) {
         this.nombre = nombre;
         this.cedula = cedula;
         this.direccion = direccion;
         this.clave = clave;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
     
     public boolean abrirCuenta(String tipo){
-        return false;
+        
+        double aux, aux2;
+        
+        boolean var = false, generado = false;
+        
+        while (!generado) {
+            
+            aux2 = Math.random()*100;
+            
+            if (!this.numCuenta.contains(aux2)) {
+            
+                this.numCuenta.add(aux2);
+                
+                generado = true;
+                
+                if (tipo.equalsIgnoreCase("corriente")) {
+                    
+                    this.bank.crearCuentaCorriente(aux2, new Cliente(this.nombre,this.cedula,this.direccion,this.clave));
+                    
+                    var = true;
+                    
+                }
+            
+            }
+        
+        }
+        
+        return var;
+          
     }
     public boolean pedirTarjeta(String tipo){
+        
+        
+        
         return false;
     }
     public boolean pedirPrestamo(double cantidad){
+        
+        
+        
+        
         return false;
     }
     
